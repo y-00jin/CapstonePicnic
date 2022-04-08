e76baad04df767361ab13364abd1e54db969de44
y-00jin <81798918+y-00jin@users.noreply.github.com>
2022-04-08 19:00:36 +0900

    윶

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,7 +1,6 @@
-import {useState} from "react";
-// import { Link } from "react-router-dom";
+
 import '../resoure/css/Calendar.css'
-import moment from "moment";
+
 
 function Calendar(){
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,33 +1,31 @@
 import React from "react";
 import logo from '../resoure/image/Logo.png';
-import headerLogo from '../resoure/image/spring1.png';
+import headerLogo from '../resoure/image/spring.png';
 import '../resoure/css/Main.css';
 import { Link } from "react-router-dom";
 
 
-function Main(){
-    return(
-            
-            <div className="App">
+function Main() {
+    return (
+
+        <div className="Main">
 
             <header className="Main-header">
-                <p>
-                <img src={headerLogo} className="Main-headerLogo" alt="headerLogo" />    
-                    </p>
-                
+                <img src={headerLogo} className="Main-headerLogo" alt="headerLogo" />
             </header>
 
             <body className="Main-body">
-
-
-            <p>' 너, 나들이 '<hr className="Main-hr"/><hr className="Main-hr"/></p>
-            <img src={logo} className="Main-bodyLogo" alt="bodyLogo" />
-            <Link to="/Calendar">
-                <button className="Main-btn Main-btn-color">다음</button>
-            </Link>
-
-
+                <p>' 너, 나들이 '<hr className="Main-hr" /><hr className="Main-hr" /></p>
+                <img src={logo} className="Main-bodyLogo" alt="bodyLogo" />
             </body>
+
+            <footer className="Main-footer">
+                <Link to="/Calendar">
+                    <button className="Main-btn Main-btn-color">다음</button>
+                </Link>
+
+            </footer>
+
         </div>
 
     );
