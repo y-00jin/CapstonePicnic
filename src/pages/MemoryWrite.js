83ab2292398847b1d7f15078401f4eeeb06ee8ff
sharon0511 <sharon0511@naver.com>
2022-04-10 21:12:38 +0900

    헤더 css 추가

--- src/pages/MemoryWrite.js
+++ src/pages/MemoryWrite.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../resoure/css/MemoryWrite.css';
 import 'C:/Capstone/picnic-front/src/sass/custom.css/custom.css';
-import '../resoure/css/PhotoAlbum.css'
+import '../resoure/css/Header.css'
+import '../resoure/css/MemoryWrite.css';
 
 function MemoryWrite(){
     return(
-        <>
-        {/* 타이틀 바 */}
-        <div className="container">
+        <div class="container">
+        
+            {/* 타이틀 바 */}
             <p/>
             <hr className="hr" />
-        
             <div className="title">
                 <Link to="/Calendar">
                     <button className="title-btn">＜</button>
@@ -21,52 +20,49 @@ function MemoryWrite(){
                 <h3></h3>
             </div>
             <hr className="hr" />
-        </div>
 
-        {/* 여행 기록 부분 */}
-        <div class="container">
-            <div class="Memory-record-font">
-                <p>　</p>
-                <div class="input-group input-group-lg">
-                    <span class="input-group-text" id="basic-addon1">여행 날짜</span>
-                    <input type="text" readonly class="form-control" id="date" value="2022-04-09"/>
-                </div>
-                <p/>
-                <div class="input-group input-group-lg">
-                    <span class="input-group-text" id="basic-addon1">여행 장소</span>
-                    <input type="text" className="form-control" placeholder=" " aria-label="place" aria-describedby="basic-addon1"/>
-                </div>
-                <p/>
-                <div class="input-group input-group-lg">
-                    <span class="input-group-text">여행 기록</span>
-                    <textarea class="form-control" aria-label="record"></textarea>
-                </div>
+            {/* 여행 기록 부분 */}
+            <p/>
+            <div class="input-group input-group-lg">
+                <span class="input-group-text" id="basic-addon1">여행 날짜</span>
+                <input type="text" readonly class="form-control" id="date" value="2022-04-09"/>
+            </div>
+            <p/>
+
+            <div class="input-group input-group-lg">
+                <span class="input-group-text" id="basic-addon1">여행 장소</span>
+                <input type="text" className="form-control" placeholder=" " aria-label="place" aria-describedby="basic-addon1"/>
+            </div>
+            <p/>
+
+            <div class="input-group input-group-lg">
+                <span class="input-group-text">여행 기록</span>
+                <textarea class="form-control" aria-label="record"></textarea>
+            </div>
+            <p>　</p>
+
+            {/* 탭 버튼 */}
+            <div class="Memory-button">
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#post">게시물</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#picture">사진</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#video">동영상</a>
+                    </li>
+                </ul>
+            </div>
+
+            {/* 탭 내용 */}
+            <div class="tab-content">
+                <div class="tab-pane container active" id="post">게시물</div>
+                <div class="tab-pane container" id="picture">사진</div>
+                <div class="tab-pane container" id="video">동영상</div>
             </div>
         </div>
-        <p>　</p>
-
-        {/* 탭 버튼 */}
-        <div class="Memory-button">
-        <ul class="nav justify-content-center">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#post">게시물</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#picture">사진</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#video">동영상</a>
-            </li>
-        </ul>
-        </div>
-
-        <div class="tab-content">
-            <div class="tab-pane container active" id="post">게시물</div>
-            <div class="tab-pane container" id="picture">사진</div>
-            <div class="tab-pane container" id="video">동영상</div>
-        </div>
-
-        </>
     );
 }
 
