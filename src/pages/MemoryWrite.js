54dcddfc5a94fb11abccf9a38a13b9138249648b
sharon0511 <sharon0511@naver.com>
2022-04-27 18:59:15 +0900

    수정!
    수정햇음

--- src/pages/MemoryWrite.js
+++ src/pages/MemoryWrite.js
@@ -5,34 +5,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'C:/Capstone/picnic-front/src/sass/custom.css/custom.css';
 import '../resoure/css/Header.css'
 import '../resoure/css/MemoryWrite.css';
-import Post from './TabContents/Post';
-import Photo from './TabContents/Photo';
-import Video from './TabContents/Video';
+import 'C:/Capstone/picnic-front/src/resoure/css/PhotoAlbum.css'
 
-const obj = {
-    0: <Post/>,
-    1: <Photo />,
-    2: <Video />,
-};
+import add from 'C:/Capstone/picnic-front/src/resoure/image/add.jpg'
+import img1 from 'C:/Capstone/picnic-front/src/resoure/image/1.jpg';
+import img2 from 'C:/Capstone/picnic-front/src/resoure/image/2.jpg';
+import img3 from 'C:/Capstone/picnic-front/src/resoure/image/3.jpg';
+import img4 from 'C:/Capstone/picnic-front/src/resoure/image/4.jpg';
+import img5 from 'C:/Capstone/picnic-front/src/resoure/image/5.jpg';
+import img6 from 'C:/Capstone/picnic-front/src/resoure/image/6.jpg';
+
+// const obj = {
+//     0: <Post/>,
+//     1: <Photo />,
+//     2: <Video />,
+// };
   
-const arr = ["First", "Second", "Third"];
+// const arr = ["First", "Second", "Third"];
   
-{arr.map((str, idx) => {
-              return (
-                <li key={str} onClick={() => this.clickHandler(idx)}>
-                  {str}
-                </li>
-              );
-            })}
+// {arr.map((str, idx) => {
+//               return (
+//                 <li key={str} onClick={() => this.clickHandler(idx)}>
+//                   {str}
+//                 </li>
+//               );
+//             })}
 
 export default class MemoryWrite extends Component {
-    state = {
-      activeId: 0,
-    };
+    // state = {
+    //   activeId: 0,
+    // };
   
-    clickHandler = (id) => {
-      this.setState({ activeId: id });
-    };
+    // clickHandler = (id) => {
+    //   this.setState({ activeId: id });
+    // };
   
     render() {
       return (
@@ -45,7 +51,7 @@ export default class MemoryWrite extends Component {
             <Link to="/Calendar">
                 <button className="title-btn">＜</button>
             </Link>
-            <h3>여행 쓰기</h3>
+            <h3>추억 쓰기</h3>
             <h3></h3>
         </div>
         <hr className="hr" />
@@ -71,15 +77,18 @@ export default class MemoryWrite extends Component {
         <p>　</p>
 
    
-        {/* 탭 버튼 활성화 */}
-        <div className="wrapper">
-          <ul class="nav justify-content-center">
-            <li class="Memory-button" onClick={() => this.clickHandler(0)}>게시물　　</li>
-            <li class="Memory-button" onClick={() => this.clickHandler(1)}>사진　　</li>
-            <li class="Memory-button" onClick={() => this.clickHandler(2)}>동영상　　</li>
-          </ul>
-          <p/>
-          <div className="contents">{obj[this.state.activeId]}</div>
+        <div id="addPost"><h4>| 추억추가</h4></div>
+
+        <p/>
+        
+        <div class="photo-layout">
+            <img className="phone-image" alt="iPhone_00" src={add} />
+            <img className="phone-image" alt="iPhone_01" src={img1} />
+            <img className="phone-image" alt="iPhone_02" src={img2} />
+            <img className="phone-image" alt="iPhone_03" src={img3} />
+            <img className="phone-image" alt="iPhone_04" src={img4} />
+            <img className="phone-image" alt="iPhone_05" src={img5} />
+            <img className="phone-image" alt="iPhone_06" src={img6} />  
         </div>
 
         {/* 버튼 */}
