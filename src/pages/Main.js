b0045d7399916f47c7408c33fc9c693800750e5f
sharon0511 <sharon0511@naver.com>
2022-05-01 20:56:44 +0900

    로그인
    로그인 컴포넌트 추가 및 src 폴더 안에 스크롤 애니메이션 예시(Animation.html) 넣어놨습니당

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -21,13 +21,44 @@ function Main() {
                 <img src={logo} className="Main-bodyLogo" alt="bodyLogo" />
             </body>
 
+            {/* 로그인 */}
+            <body class="text-center">
+                <main className="Main-login">
+                    <form>
+                        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+                        <div class="form-floating">
+                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+                            <label for="floatingInput">아이디를 입력해주세요</label>
+                        </div>
+    
+                        <div class="form-floating">
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
+                            <label for="floatingPassword">비밀번호를 입력해주세요</label>
+                        </div>
+
+                        {/* 사용자 기억 기능 임시..폐쇄 */}
+                        {/* <div class="checkbox mb-3">
+                            <label>
+                                <input type="checkbox" value="remember-me"/> Remember me
+                            </label>
+                        </div> */}
+    
+                        <footer className="Main-footer">
+                            <Link to="/Calendar">
+                                <button className="Main-btn Main-btn-color">로그인</button>
+                            </Link>
+                        </footer>
+                    </form>
+                </main>
+            </body>
+
             {/* 다음 버튼 */}
-            <footer className="Main-footer">
+            {/* <footer className="Main-footer">
                 <Link to="/Calendar">
                     <button className="Main-btn Main-btn-color">다음</button>
                 </Link>
-
-            </footer>
+            </footer> */}
 
         </div>
 
