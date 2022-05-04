027ed66dc79a0a32564e643bcf0aba1e4ca76e13
sharon0511 <sharon0511@naver.com>
2022-05-04 10:56:06 +0900

    수업중 짬내서 수정
    햇음용ㅋ

--- src/pages/TabContents/Photo.js
+++ src/pages/TabContents/Photo.js
@@ -1,4 +1,4 @@
-import 'C:/Capstone/picnic-front/src/resoure/css/PhotoAlbum.css'
+import 'C:/Capstone/picnic-front/src/resoure/css/Tab.css'
 
 import img1 from 'C:/Capstone/picnic-front/src/resoure/image/1.jpg';
 import img2 from 'C:/Capstone/picnic-front/src/resoure/image/2.jpg';
@@ -6,10 +6,10 @@ import img3 from 'C:/Capstone/picnic-front/src/resoure/image/3.jpg';
 
 function Photo() {
     return(
-        <div class="photo-layout">
-            <img className="phone-image" alt="iPhone_01" src={img1} />
-            <img className="phone-image" alt="iPhone_02" src={img2} />
-            <img className="phone-image" alt="iPhone_03" src={img3} />
+        <div class="tab-photo-layout">
+            <img className="tab-phone-image" alt="iPhone_01" src={img1} />
+            <img className="tab-phone-image" alt="iPhone_02" src={img2} />
+            <img className="tab-phone-image" alt="iPhone_03" src={img3} />
         </div>
     )
 }
--- src/pages/TabContents/Video.js
+++ src/pages/TabContents/Video.js
@@ -1,4 +1,4 @@
-import 'C:/Capstone/picnic-front/src/resoure/css/PhotoAlbum.css'
+import 'C:/Capstone/picnic-front/src/resoure/css/Tab.css'
 
 import img4 from 'C:/Capstone/picnic-front/src/resoure/image/4.jpg';
 import img5 from 'C:/Capstone/picnic-front/src/resoure/image/5.jpg';
@@ -6,10 +6,10 @@ import img6 from 'C:/Capstone/picnic-front/src/resoure/image/6.jpg';
 
 function Video() {
     return(
-        <div class="photo-layout">
-            <img className="phone-image" alt="iPhone_04" src={img4} />
-            <img className="phone-image" alt="iPhone_05" src={img5} />
-            <img className="phone-image" alt="iPhone_06" src={img6} />
+        <div class="tab-photo-layout">
+            <img className="tab-phone-image" alt="iPhone_04" src={img4} />
+            <img className="tab-phone-image" alt="iPhone_05" src={img5} />
+            <img className="tab-phone-image" alt="iPhone_06" src={img6} />
         </div>
     )
 }
