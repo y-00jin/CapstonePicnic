50a8e638a71ec3a6ac0242102e4c582c6d7ebf58
sharon0511 <sharon0511@naver.com>
2022-05-18 17:49:17 +0900

    충돌 나서 수정ㅠ

--- src/pages/TabContents/Post.js
+++ src/pages/TabContents/Post.js
@@ -12,24 +12,12 @@ import img6 from 'C:/Capstone/picnic-front/src/resoure/image/6.jpg';
 function Post() {
     return(
         <div className="tab-photo-layout">
-            <Link to="/Details">
-                <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_01" src={img1} /></button>
-            </Link>
-            <Link to="/Details">
-                <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_02" src={img2} /></button>
-            </Link>
-            <Link to="/Details">
-                <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_03" src={img3} /></button>
-            </Link>
-            <Link to="/Details">
-                <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_04" src={img4} /></button>
-            </Link>
-            <Link to="/Details">
-                <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_05" src={img5} /></button>
-            </Link>
-            <Link to="/Details">
-                <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_06" src={img6} /></button>
-            </Link>
+            <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_01" src={img1} /></button>
+            <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_02" src={img2} /></button>
+            <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_03" src={img3} /></button>
+            <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_04" src={img4} /></button>
+            <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_05" src={img5} /></button>
+            <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_06" src={img6} /></button>
         </div>
     )
 }
--- src/pages/TabContents/Video.js
+++ src/pages/TabContents/Video.js
@@ -8,15 +8,9 @@ import img6 from 'C:/Capstone/picnic-front/src/resoure/image/6.jpg';
 function Video() {
     return(
         <div class="tab-photo-layout">
-            <Link to="/Details">
-                <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_04" src={img4} /></button>
-            </Link>
-            <Link to="/Details">
-                <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_05" src={img5} /></button>
-            </Link>
-            <Link to="/Details">
-                <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_06" src={img6} /></button>
-            </Link>
+           <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_04" src={img4} /></button>
+            <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_05" src={img5} /></button>
+            <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_06" src={img6} /></button>
         </div>
     )
 }
