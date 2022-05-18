8c2d3377af145c2e8dbe3fb23d1458981f16fb06
sharon0511 <sharon0511@naver.com>
2022-05-18 17:50:10 +0900

    했는데 또 충돌ㅠ

--- src/pages/TabContents/Video.js
+++ src/pages/TabContents/Video.js
@@ -8,7 +8,7 @@ import img6 from 'C:/Capstone/picnic-front/src/resoure/image/6.jpg';
 function Video() {
     return(
         <div class="tab-photo-layout">
-           <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_04" src={img4} /></button>
+            <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_04" src={img4} /></button>
             <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_05" src={img5} /></button>
             <button className="tab-button" type="button"><img className="tab-phone-image" alt="iPhone_06" src={img6} /></button>
         </div>
